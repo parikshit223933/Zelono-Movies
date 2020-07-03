5b283a04409eaeaaae9d714ff2af9c4603bdc240
Parikshit Singh <pk223933@gmail.com>
2020-07-03 17:08:35 +0530

    Updates

---
+++ precache-manifest.55b0ccd54d575af85c1d0c699fb5a9db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75787bd4c89df2ed206dbd0e4c003120",
+    "url": "/Zelono-Movies/index.html"
+  },
+  {
+    "revision": "18e9c2561a09ca49e4aa",
+    "url": "/Zelono-Movies/static/css/main.2e5ac4c4.chunk.css"
+  },
+  {
+    "revision": "7b659a243de36ef843f5",
+    "url": "/Zelono-Movies/static/js/2.3b505510.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Zelono-Movies/static/js/2.3b505510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e9c2561a09ca49e4aa",
+    "url": "/Zelono-Movies/static/js/main.9a82eca4.chunk.js"
+  },
+  {
+    "revision": "07b46901171c02a8ad52",
+    "url": "/Zelono-Movies/static/js/runtime-main.1da2adbc.js"
+  }
+]);
